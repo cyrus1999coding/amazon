a036f07cf403b98365b737a45e51f31ddbbf5eaf
cyrus1999coding <cryrus1999coding@gmail.com>
2025-03-08 20:12:12 +0330

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -53,6 +53,32 @@ class clothing extends Product {
   }
 }
 
+/*
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
+// console.log(this);
+
+// const object2 = {
+//   a: 2,
+//   b: this.a,
+// };
+
+// function logThis() {
+//   console.log(this);
+// }
+// logThis();
+// logThis.call("hello");
+
+// this;
+// const object3 = {
+//   mehthod: () => {
+//     console.log(this);
+//   },
+// };
+// object3.mehthod();
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6123",
