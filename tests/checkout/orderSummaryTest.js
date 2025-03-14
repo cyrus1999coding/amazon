c77e66d3f80af675efa2aa0781dbf8e0505c9f04
cyrus1999coding <cryrus1999coding@gmail.com>
2025-03-14 22:45:09 +0330

    Load products in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,10 +1,15 @@
 import { renderOrderSummary } from "../../scripts/checkout/ordersummary.js";
 import { cart } from "../../data/cart-class.js";
+import { loadProducts } from "../../data/products.js";
 
 describe("test suite: renderOrderSummary", () => {
   const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
   const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
-
+  beforeAll((done) => {
+    loadProducts(() => {
+      done();
+    });
+  });
   beforeEach(() => {
     spyOn(localStorage, "setItem");
 
@@ -95,7 +100,7 @@ describe("test suite: renderOrderSummary", () => {
 
     expect(cart.cartItems.length).toEqual(2);
     expect(cart.cartItems[0].productId).toEqual(productId1);
-    expect(cart.cartItems[0].deliveryOptionId).toEqual("1");
+    expect(cart.cartItems[0].deliveryOptionId).toEqual("3");
 
     expect(
       document.querySelector(".js-payment-summary-shipping").innerText
