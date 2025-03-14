d3fd9406f8bc9564dc182d8a899adfc8e9661baf
cyrus1999coding <cryrus1999coding@gmail.com>
2025-03-14 13:19:15 +0330

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,9 @@
 import { renderOrderSummary } from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
+import "../data/backend-practice.js";
 // import "../data/cart-class.js";
-import "../data/car.js"
+// import "../data/car.js";
 
 renderOrderSummary();
 renderPaymentSummary();
