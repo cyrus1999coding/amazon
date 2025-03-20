348f74f6de660b283ac86856f455996f78105321
cyrus1999coding <cryrus1999coding@gmail.com>
2025-03-20 16:53:37 +0330

    Making all amazon.html filepath to index.html in order to make github action

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -143,7 +143,7 @@ function renderProductsGrid() {
   updateCartQuantity();
   document.querySelector(".js-search-button").addEventListener("click", () => {
     const search = document.querySelector(".js-search-bar").value;
-    window.location.href = `amazon.html?search=${search}`;
+    window.location.href = `index.html?search=${search}`;
   });
 
     // Extra feature: searching by pressing "Enter" on the keyboard.
@@ -151,7 +151,7 @@ function renderProductsGrid() {
     .addEventListener('keydown', (event) => {
       if (event.key === 'Enter') {
         const searchTerm = document.querySelector('.js-search-bar').value;
-        window.location.href = `amazon.html?search=${searchTerm}`;
+        window.location.href = `index.html?search=${searchTerm}`;
       }
     });
 }
